08d6a810ad8c2d1713d3c92c28c9643cf656f254
aasim-akhtar <xpaasim@gmail.com>
2021-04-11 19:44:45 +0530

    connected spinner to adapterCourses

--- app/src/main/java/com/aasimakhtar/notekeeper/NoteActivity.java
+++ app/src/main/java/com/aasimakhtar/notekeeper/NoteActivity.java
@@ -29,7 +29,8 @@ public class NoteActivity extends AppCompatActivity {
         Spinner spinnerCourses= findViewById(R.id.spinner_courses);
         List<CourseInfo> courses= DataManager.getInstance().getCourses();
         ArrayAdapter<CourseInfo> adapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,courses);
-
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(adapterCourses);
     }
 
     @Override
